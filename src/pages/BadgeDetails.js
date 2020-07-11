5efa410bbb10e046e5ec807b736a0347774cbcaa
abdiel249 <abdielorellana3@gmail.com>
2020-07-10 21:08:20 -0400

    example custom HOOKS

--- src/pages/BadgeDetails.js
+++ src/pages/BadgeDetails.js
@@ -6,8 +6,18 @@ import Logo from '../images/platziconf-logo.svg'
 import Badge from '../components/Badge'
 import DeleteBadgeModal from '../components/DeleteBadgeModal'
 
+
+function useIncreaseCount(max){
+  const [ count, setCount ] = React.useState(0)
+  if(count > max){
+    setCount(0)
+  }
+  return [count, setCount]
+}
+
 function BadgeDetails ( props ){
 
+  const [ count, setCount ] = useIncreaseCount(5)
   const badge = props.badge
   
   return (
@@ -41,6 +51,13 @@ function BadgeDetails ( props ){
             <div className="col">
               <h2>Actions</h2>
               <div>
+                <button
+                  onClick={() => {
+                    setCount(count + 1)
+                  }}
+                  className="btn btn-primary mr-3">
+                  Increase Count: {count}
+                </button>
                 <Link
                   className="btn btn-primary mb-4" 
                   to={`/badges/${badge.id}/edit`}>
